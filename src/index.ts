30594da78849274854e069d705fbdd3e0605667b
Vyacheslav Pukhanov <vyacheslav@pukhanov.ru>
2021-11-14 09:10:49 +0300

    add babel type imports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('test')
+import type typesNs from '@babel/types'
+import type templateFn from '@babel/template'
+
+export default function flushableImportPlugin({
+  types: t,
+  template,
+}: {
+  types: typeof typesNs
+  template: typeof templateFn
+}) {}
