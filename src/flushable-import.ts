0d0589539249822cd6c1e2796c4060e211a49c72
Vyacheslav Pukhanov <vyacheslav@pukhanov.ru>
2021-11-14 12:01:58 +0300

    implement flushable import fn

--- src/flushable-import.ts
+++ src/flushable-import.ts
@@ -1,1 +1,20 @@
-export default function flushableImport() {}
+type FlushableImportConfig<T> = {
+  load: () => Promise<T>
+  resolve: () => string | number
+  path: () => string
+  chunkName: string
+}
+
+export default function flushableImport<T>(
+  config: FlushableImportConfig<T>
+): FlushableImportConfig<T> & {
+  then: (cb: (mod: T) => void) => void
+  catch: (cb: (err: Error) => void) => void
+} {
+  const { load } = config
+  return {
+    ...config,
+    then: cb => load().then(mod => cb(mod)),
+    catch: cb => load().catch(err => cb(err)),
+  }
+}
